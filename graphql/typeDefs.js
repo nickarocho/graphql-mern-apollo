e8e95d2ab8529081912e6acf28ec6bdbdf89b4aa
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-26 19:26:54 -0700

    Restructure resolvers so app is more scalable

---
+++ graphql/typeDefs.js
@@ -0,0 +1,15 @@
+// const { default: gql } = require('graphql-tag');
+
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
