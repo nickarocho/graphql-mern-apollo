710e9617bbca50837bb9f77b2285ed4b18ad4421
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-26 20:02:28 -0700

    Add user authentication with bcrypt and JWT's

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { SECRET_KEY } = require('../../config');
 const User = require('../../models/user');
 
 module.exports = {
   Mutation: {
-    register(_, args, ctx, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      ctx,
+      info
+    ) {
       // TODO: Validate user data
       // TODO: Make sure user doesn't already exist
       // TODO: Hash the password
+      password = await bcrypt.hash(password, 12);
+
+      // create the new user with the model and passed in data
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      // save the user to the DB
+      const res = await newUser.save();
       // TODO: Create auth token
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
