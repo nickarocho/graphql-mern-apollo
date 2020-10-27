eae2b7b829c554f97f730f060bd11e58a7da33de
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-26 20:10:21 -0700

    Add server-side logic to check if user already exists

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/user');
@@ -8,13 +9,20 @@ module.exports = {
   Mutation: {
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      ctx,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
       // TODO: Validate user data
       // TODO: Make sure user doesn't already exist
-      // TODO: Hash the password
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken... sorry yo.',
+          },
+        });
+      }
+
+      // Hash the password
       password = await bcrypt.hash(password, 12);
 
       // create the new user with the model and passed in data
@@ -27,7 +35,7 @@ module.exports = {
 
       // save the user to the DB
       const res = await newUser.save();
-      // TODO: Create auth token
+      // Create auth token
       const token = jwt.sign(
         {
           id: res.id,
