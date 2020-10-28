ee022ff192a1ebd349ebf35fc4280e6bf1c3e5f0
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-28 16:17:29 -0700

    formatting stuff

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,21 +1,18 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
 import App from './App';
-import { ApolloClient } from '@apollo/client';
-import { InMemoryCache } from '@apollo/client';
-import { createHttpLink } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'http://localhost:5000',
 });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export default (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-)
+);
