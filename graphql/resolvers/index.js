2b0bab4fb1a3afae7af2cffff7b4412dd8709bec
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-27 17:12:12 -0700

    Compute likeCount and commentCount on the server with a modifier

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,11 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
+  // modifier - the name of the type, and each time ANY mutation or subscription that returns a post, it will go through this modifier and apply these modifications
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
