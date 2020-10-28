acfb74faf6d0a56d0bae7ef4d2eac3ee0bacbc0d
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-28 16:47:28 -0700

    Add React Router, add Semantic UI, create a MenuBar with some simple links to new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
