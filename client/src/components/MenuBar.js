7df03261281cca03478802389ebb3899717971dd
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-29 21:24:26 -0700

    Dynamically render menu based on logged in state

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   // dynamically set the 'active' link by looking at the current URL
   const pathName = window.location.pathname;
   const path = pathName === '/' ? 'home' : pathName.substr(1);
@@ -10,9 +13,16 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menuBar = user ? (
     <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item name="home" active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} as={Link} to="/login" />
+      </Menu.Menu>
+    </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name="home" active={activeItem === 'home'} onClick={handleItemClick} />
       <Menu.Menu position="right">
         <Menu.Item name="login" active={activeItem === 'login'} onClick={handleItemClick} as={Link} to="/login" />
         <Menu.Item
@@ -25,6 +35,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 }
 
 export default MenuBar;
