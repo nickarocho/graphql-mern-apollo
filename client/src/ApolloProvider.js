a67a7049e65a9beae2dcffa5309ad5cc9340295d
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-30 15:52:08 -0700

    Add middleware to add Auth token to API calls

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000',
 });
 
+// kind of a middleware to add authorization headers to API calls
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
