9d48d290c7e4d8883192ad20c8b4c64faac59913
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-30 15:52:47 -0700

    Add ability for logged in user to create a new post, and leverage Apollo cache to reuse DB queries/mutations

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+import { useForm } from '../util/hooks';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: '',
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          getPosts: [result.data.createPost, ...data.getPosts],
+        },
+      });
+      values.body = '';
+    },
+    onError(err) {
+      return err;
+    },
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a Post</h2>
+      <Form.Field>
+        <Form.Input placeholder="Hi, World!" name="body" onChange={onChange} value={values.body} />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  );
+}
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
+    }
+  }
+`;
+
+export default PostForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
 /* eslint-disable no-use-before-define */
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
 import { Grid, Image } from 'semantic-ui-react';
 
+import { AuthContext } from '../context/auth';
+
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext);
 
   return (
     <Grid columns={3}>
@@ -16,6 +20,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
@@ -30,26 +39,5 @@ function Home() {
     </Grid>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
