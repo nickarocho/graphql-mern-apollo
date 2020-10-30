9cc82d9a5591711398b9fdb5e6f3c049dc07235b
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-29 18:39:45 -0700

    Create form and GQL mutation to register users

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,90 @@
-import React from 'react';
+/* eslint-disable no-use-before-define */
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react';
 
 function Register() {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(proxy, result) {
+      // eslint-disable-next-line no-console
+      console.log({ result });
+    },
+    variables: values,
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addUser();
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
+        <h1>Register</h1>
+        <Form.Input
+          type="text"
+          label="Username"
+          placeholder="Username..."
+          name="username"
+          value={values.username}
+          onChange={onChange}
+        />
+        <Form.Input
+          type="email"
+          label="Email"
+          placeholder="Email..."
+          name="email"
+          value={values.email}
+          onChange={onChange}
+        />
+        <Form.Input
+          type="password"
+          label="Password"
+          placeholder="Password..."
+          name="password"
+          value={values.password}
+          onChange={onChange}
+        />
+        <Form.Input
+          type="password"
+          label="Confirm Password"
+          placeholder="Confirm Password..."
+          name="confirmPassword"
+          value={values.confirmPassword}
+          onChange={onChange}
+        />
+        <Button type="submit" primary>
+          Register
+        </Button>
+      </Form>
     </div>
   );
 }
+
+const REGISTER_USER = gql`
+  mutation register($username: String!, $email: String!, $password: String!, $confirmPassword: String!) {
+    register(
+      registerInput: { username: $username, email: $email, password: $password, confirmPassword: $confirmPassword }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default Register;
